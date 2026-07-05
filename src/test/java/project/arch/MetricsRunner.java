9d09d68973b8a3f1432439c528d050b3e101a0ad
antoniomoltelo1205 <a.moltelo@studenti.unina.it>
2026-07-05 20:06:16 +0200

    Update MetricsRunner.java

--- src/test/java/project/arch/MetricsRunner.java
+++ src/test/java/project/arch/MetricsRunner.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class MetricsRunner {
     public static void main(String[] args) {
         JavaClasses classes = new ClassFileImporter().withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS).importPackages("");
-        Set<JavaPackage> packages = classes.getPackage("project").getSubpackages();
+        Set<JavaPackage> packages = classes.getDefaultPackage().getSubpackages();
 
         MetricsComponents<JavaClass> components = MetricsComponents.fromPackages(packages);
 
